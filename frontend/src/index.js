b93c8050f2517c6ff12bff216a724e13e3b9ba9e
Atilio <atilio.jesus@grupoboticario.com.br>
2023-08-02 11:02:21 -0300

    First structure of frontend

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,14 +1,38 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import reducer, { initialState } from './reducer';
-import { StateProvider } from './StateContext';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <StateProvider initialState={initialState} reducer={reducer}>
-      <App />
-    </StateProvider>
-  </React.StrictMode>,
-  document.getElementById('root')
+import App from './App';
+import { UserProvider } from './components/contexts/user.context';
+import { CategoriesProvider } from './components/contexts/categories.context';
+import { CartProvider } from './components/contexts/cart.context';
+
+// STRIPE
+import { Elements } from '@stripe/react-stripe-js';
+import { stripePromise } from './utils/stripe/stripe.utils';
+
+import './index.css';
+
+// import reportWebVitals from './reportWebVitals';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <React.StrictMode>
+        <BrowserRouter>
+            <UserProvider>
+                <CategoriesProvider>
+                    <CartProvider>
+                        <Elements stripe={stripePromise}>
+                            <App />
+                        </Elements>
+                    </CartProvider>
+                </CategoriesProvider>
+            </UserProvider>
+        </BrowserRouter>
+    </React.StrictMode>
 );
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// reportWebVitals(console.log);
