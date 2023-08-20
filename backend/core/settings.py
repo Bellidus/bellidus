e02b32376979b13583bf59d75647d9d40034ed07
Atilio <atilio.jesus@grupoboticario.com.br>
2023-08-20 19:54:14 -0300

    Remove comments and put debug as false

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'e^@o6^p&p#%_ucy(8yo)!2x4+ev3b3^1u)us+a42py!@wkce)='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
